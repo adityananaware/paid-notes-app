c10cf0c063d1ef37b53dec96f8e06db76dcbd019
Aditya Nanaware <adityananaware2@gmail.com>
2025-06-01 23:25:23 +0530

    Add files via upload

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+
+export default NextAuth({
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }),
+    // You can add more providers (Google, Credentials, etc.)
+  ],
+  // Add custom callbacks, session, and database config as needed
+});
